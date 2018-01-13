889c55fb7aca6579172f9425139fbe35bb62dd4f
Viktor Pyskunov <vicpi89@gmail.com>
2018-01-13 10:23:09 +0100

    Repository: extract duplicated logic into queryDatabase method

--- src/repository.js
+++ src/repository.js
@@ -1,6 +1,5 @@
 // @flow
 
-
 export type TList = {
     uuid: string,
     name: string
@@ -13,53 +12,49 @@ export type TConnection = any
 type TCriteria = any
 
 class Repository {
-    connection: TConnection
-    model: any
-
-    constructor(connection: TConnection, model: any) {
+    connection: TConnection model: any constructor(connection : TConnection, model : any) {
         this.connection = connection
         this.model = model
     }
 
-    findAll(): TPromise {
+    generateQuery(criteria : TCriteria = null) {
+        const parts = []
+        parts.push(`select * from ${this.model.table}`)
+        if (criteria !== null) {
+            parts.push(criteria)
+        }
+        return parts.join(' ')
+    }
+
+    queryDatabase(query : string) {
         return new Promise((resolve, reject) => {
-            const query = `select * from ${this.model.table}`
             console.log('---------------------- SELECT ------------------')
             console.log(query)
             console.log('------------------------------------------------')
-            this.connection.query(`select * from ${this.model.table}`,
-                (error, results, fields) => {
-                    if (error) {
-                        reject(error);
-                    }
-                    const models = []
-                    for (let item of results) {
-                        models.push(new this.model(item))
-                    }
-                    resolve(models)
-                });
+            this.connection.query(query, (error, results, fields) => {
+                if (error) {
+                    reject(error);
+                }
+                const models = []
+                for (let item of results) {
+                    models.push(new this.model(item))
+                }
+                resolve(models)
+            });
         })
     }
 
-    find(criteria: TCriteria): TPromise {
-        return new Promise((resolve, reject) => {
-            const query = `select * from ${this.model.table} ${criteria}`
-            console.log('---------------------- SELECT ------------------')
-            console.log(query)
-            console.log('------------------------------------------------')
-            this.connection.query(query,
-                (error, results, fields) => {
-                    if (error) {
-                        reject(error);
-                    }
-                    const models = []
-                    for (let item of results) {
-                        models.push(new this.model(item))
-                    }
-                    resolve(models)
-                });
-        })
+    findAll(): TPromise {
+        const query = this.generateQuery()
+        return this.queryDatabase(query)
+    }
+
+    find(criteria : TCriteria): TPromise {
+        const query = this.generateQuery(criteria)
+        return this.queryDatabase(query)
     }
 }
 
-export {Repository}
+export {
+    Repository
+}
