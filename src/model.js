8ff4056baa350513bd72fc4afcf21d63f75ad6ce
Viktor Pyskunov <vicpi89@gmail.com>
2018-01-14 10:06:15 +0100

    Foreign key: getItemList

--- src/model.js
+++ src/model.js
@@ -1,9 +1,26 @@
+import {Criteria} from './criteria'
+import {ORM} from './orm'
+import {expr, quote} from './expression'
+import './polyfill'
+
 class Model {
-    constructor(databaseRow: any) {
+    constructor(databaseRow : any) {
         const fields = this.getFields()
         fields.forEach(modelField => {
             const databaseField = this.constructor[modelField]
             this[modelField] = databaseRow[databaseField]
+        })
+
+        const foreignKeys = this.constructor.getForeignKeys()
+        foreignKeys.forEach(foreignKeyName => {
+            const foreignKey = this.constructor[foreignKeyName]
+            const getListMethodName = `get${foreignKeyName.capitalize()}List`
+            this[getListMethodName] = () => {
+                const repository = ORM.createRepository(this.constructor)
+                const criteria = new Criteria().where(expr(foreignKey.foreignModel[foreignKey.key], '=', quote(this.id)))
+                const query = repository.generateQuery(foreignKey.foreignModel, criteria)
+                return repository.queryDatabase(query, foreignKey.foreignModel)
+            }
         })
     }
 
@@ -22,17 +39,15 @@ class Model {
     getFields() {
         const reservedProperties = this.getReservedProperties()
         // console.log('keys ', Object.keys(this.constructor), Object.values(this.constructor));
-        const fields = Object.keys(this.constructor)
-            .filter(item => !reservedProperties.includes(item))
-            .filter(item => !this.constructor.getForeignKeys().includes(item))
+        const fields = Object.keys(this.constructor).filter(item => !reservedProperties.includes(item)).filter(item => !this.constructor.getForeignKeys().includes(item))
         return fields
     }
 
     static getForeignKeys() {
-        return Object.keys(this)
-            .filter(key => typeof this[key] === 'object')
-            .map(key => this[key])
+        return Object.keys(this).filter(key => typeof this[key] === 'object')
     }
 }
 
-export {Model}
+export {
+    Model
+}
