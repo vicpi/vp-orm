957b5a99c39f08e27d9c02f4e086e55ace8aa496
Viktor Pyskunov <vpyskunov@gmail.com>
2018-01-14 10:06:15 +0100

    Foreign key: getItemList

--- src/repository.js
+++ src/repository.js
@@ -25,10 +25,10 @@ class Repository {
             ? '*'
             : `${foreignKey.foreignModel.table}.*`
         parts.push(`select ${selectFields} from ${model.table}`)
-        if (foreignKey !== null) {
-            parts.push(`LEFT JOIN ${foreignKey.foreignModel.table}
-                    on ${model.table}.${model.id}=${foreignKey.foreignModel.table}.${foreignKey.foreignKey}`)
-        }
+        // if (foreignKey !== null) {
+            // parts.push(`LEFT JOIN ${foreignKey.foreignModel.table}
+                    // on ${model.table}.${model.id}=${foreignKey.foreignModel.table}.${foreignKey.foreignKey}`)
+        // }
         if (criteria !== null) {
             parts.push(criteria)
         }
@@ -56,25 +56,25 @@ class Repository {
     async findAll(): TPromise {
         const query = this.generateQuery(this.model)
         const models = await this.queryDatabase(query, this.model)
-        const foreignKeys = this.model.getForeignKeys()
-        if (foreignKeys.length > 0) {
-            const foreignKey = foreignKeys[0]
-            const foreignQuery = this.generateQuery(this.model, null, foreignKey)
-            const foreignModels = await this.queryDatabase(foreignQuery, foreignKey.foreignModel)
-            foreignModels.forEach(foreignModel => {
-                // console.log('foreignKey ', foreignModel, foreignModel[foreignKey.foreignModelKey]);
-                const correspondingModel = models.find(
-                    model => model.id === foreignModel[foreignKey.foreignModelKey]
-                )
-                // console.log('correspondingModel ', correspondingModel);
-                if (correspondingModel.books === undefined) {
-                    correspondingModel.books = []
-                }
-                correspondingModel.books.push(foreignModel)
-            })
-            // console.log(models);
-            // console.log(foreignModels);
-        }
+        // const foreignKeys = this.model.getForeignKeys()
+        // if (foreignKeys.length > 0) {
+        //     const foreignKey = foreignKeys[0]
+        //     const foreignQuery = this.generateQuery(this.model, null, foreignKey)
+        //     const foreignModels = await this.queryDatabase(foreignQuery, foreignKey.foreignModel)
+        //     foreignModels.forEach(foreignModel => {
+        //         // console.log('foreignKey ', foreignModel, foreignModel[foreignKey.foreignModelKey]);
+        //         const correspondingModel = models.find(
+        //             model => model.id === foreignModel[foreignKey.foreignModelKey]
+        //         )
+        //         // console.log('correspondingModel ', correspondingModel);
+        //         if (correspondingModel.books === undefined) {
+        //             correspondingModel.books = []
+        //         }
+        //         correspondingModel.books.push(foreignModel)
+        //     })
+        //     // console.log(models);
+        //     // console.log(foreignModels);
+        // }
         return models
     }
 
